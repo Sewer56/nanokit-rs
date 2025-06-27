92319730d1832153fd5f47c01f6b626a611a7727
Sewer56 <sewer56lol@googlemail.com>
2025-06-27 03:43:11 +0100

    Fixed: Benchmark lints

--- benches/my_benchmark/main.rs
+++ benches/my_benchmark/main.rs
@@ -1,7 +1,8 @@
 // Example of how to include a 2nd file.
 mod util;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 
 #[cfg(all(
     any(target_os = "linux", target_os = "macos"),
